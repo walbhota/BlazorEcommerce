048aa28de0bf4d5d0c3dccc2246b5dcc9ce6303c
j8 <walbhota@gmail.com>
2022-07-27 11:10:23 +0100

    Added Swashbuckle for swaggerUI for API viewer

--- BlazorEcommerce/Server/Controllers/ProductController.cs
+++ BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -36,7 +36,7 @@ namespace BlazorEcommerce.Server.Controllers
         };
 
         [HttpGet]
-        public async Task<IActionResult> GetProduct()
+        public async Task<ActionResult<List<Product>>> GetProduct()
         {
             return Ok(Products);
         }
